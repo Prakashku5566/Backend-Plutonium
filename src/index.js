8bff198ec01b69bbadd7c67c2a205ec614da06f5
Prakash <prakashku5566singh@gmail.com>
2022-08-13 20:31:38 +0530

    create book assigment solved

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://prakashku5566:9570938768@cluster0.wzd1jtg.mongodb.net/Prakash-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
+const bookController= require("../controllers/bookContoller")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+
+router.post("/createBook",bookController.createBook)
+
+router.get("/getbooks",bookController.getbook)
 
 router.post("/createUser", UserController.createUser  )
 
