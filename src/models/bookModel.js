1fd4ab2d41283e72ff7e7a3d03b91a28a12fe936
Prakash <prakashku5566singh@gmail.com>
2022-08-17 12:03:57 +0530

    assigment solved

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
+const bookSchema = new mongoose.Schema(
+  {
+    bookName: {
+      type: String,
+      require: true,
+    },
+    authorName: String,
     tags: [String],
-    
+
     isPublished: Boolean,
     prices: {
-        indianPrice: String,
-        europePrice: String,
+      indianPrice: String,
+      europePrice: String,
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    sales: { type: Number, default: 10 },
+    year: {
+      type: Number,
+      default: 2022,
+    },
+    TotalPage: Number,
+    stockAvailable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
-
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model("Book", bookSchema); //books
 
 //Validation:
 //require:true
