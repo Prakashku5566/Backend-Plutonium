7ef0c53837d357b12a8594ab18d702593443db67
Prakash <prakashku5566singh@gmail.com>
2022-08-28 19:52:30 +0530

    assigment solved

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://prakashku5566:9570938768@cluster0.wzd1jtg.mongodb.net/Prakash-DB1", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
